c36205713c32ee2c62ea38ea1f019d88a31b5d18
PUJA SINGH <pujasa25@gmail.com>
2020-05-14 10:23:03 +0530

    user ui for carwash app to repo

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
